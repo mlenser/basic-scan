5e9ef1d49772a10e2b273867686b0344db821f55
Mark Lenser <mlenser@gmail.com>
2024-04-20 13:15:41 +0100

    fix: input styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { AddressForm } from '@app/_/AddressForm';
 
 const Home = () => (
-  <main className="mx-auto h-full max-w-md p-10">
+  <main className="dark mx-auto h-full max-w-md p-10">
     <AddressForm />
   </main>
 );
